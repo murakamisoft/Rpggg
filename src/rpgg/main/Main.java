0d3c09c93ec3a563a9d621b2acdb168c7b45e14c
Murakami_mac <nori@vbminigame.sakura.ne.jp>
2018-12-23 19:50:43 +0900

    *

--- src/rpgg/main/Main.java
+++ src/rpgg/main/Main.java
@@ -1,10 +1,16 @@
 package rpgg.main;
 
+/**
+ * メイン
+ *
+ * @author admin
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello RPG!");
-		GameMng gameMng=new GameMng();
+		GameMng gameMng = new GameMng();
 		gameMng.execute();
 	}
 
